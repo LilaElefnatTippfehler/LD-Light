97b4133a08861889e3770484f85be47e5b92e439
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2019-04-02 17:28:23 +0200

    first

--- src/LD-Light.ino
+++ src/LD-Light.ino
@@ -1,5 +1,13 @@
 // This #include statement was automatically added by the Particle IDE.
+//#define ARDUINOJSON_ENABLE_PROGMEM 0
+//#include <ArduinoJson.h>
+
+// This #include statement was automatically added by the Particle IDE.
+#include <MQTT.h>
+
+// This #include statement was automatically added by the Particle IDE.
 #include <neopixel.h>
+
 
 
 // CONFIGURATION SETTINGS START
@@ -21,6 +29,12 @@ String sparkId[] = {
 //          (or find a way to automate it, found it hard as it is realised as preprocessor Comand)
 //Add PIN Configurations for strip in setup()
 
+byte server[] = { 192,168,0,220 };
+MQTT client(server, 1883, callback);
+#define DEVICENAME "LDL"
+#define MQTT_USR "andi"
+#define MQTT_PW "andi12"
+unsigned long lastReconnectAttempt = 0;
 
 #define SOFTWARE "SWPHALL_1.2"
 
@@ -212,6 +226,18 @@ void loop() {
                 einDurchlauf++;
         }
 
+        if (!client.isConnected()) {
+                unsigned long now = millis();
+                if (now - lastReconnectAttempt > 5000) {
+                        lastReconnectAttempt = now;
+                        // Attempt to reconnect
+                        if (reconnect()) {
+                                lastReconnectAttempt = 0;
+                        }
+                }
+        }
+        client.loop();
+
         touchEvent = touchEventCheck();
         if (touchEvent == tEVENT_TOUCH) {
                 currentEvent = touchEvent;
@@ -221,6 +247,83 @@ void loop() {
         }
         stateAndPixelMagic();
 
+
+}
+
+boolean reconnect() {
+        // Loop until we're reconnected
+        if (client.connect(String(DEVICENAME), MQTT_USR, MQTT_PW)) {
+                char buffer[100];
+                sprintf(buffer, "%s%s%s", "/actu/", DEVICENAME, "/cmd");
+                client.subscribe(buffer);
+                sprintf(buffer, "%s%s%s", "/actu/", DEVICENAME, "/color");
+                client.subscribe(buffer);
+                sprintf(buffer, "%s%s%s", "/actu/", DEVICENAME, "/brightness");
+                client.subscribe(buffer);
+                client.publish("/actu/LDL/status","connected");
+        } else {
+                Serial.print("MQTT conncetion failed,");
+                Serial.println(" try again in 5 seconds");
+        }
+        return client.isConnected();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+        client.publish("/actu/LDL/status","received");
+        char buffer[100];
+        sprintf(buffer, "%s%s%s", "/actu/", DEVICENAME, "/cmd");
+        /*if (!strcmp(buffer, topic)) {
+            const int capacity = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3);
+           StaticJsonDocument<capacity> msg;
+           DeserializationError err = deserializeJson(msg, payload);
+           if(!err) {
+            auto cmd = msg["cmd"];
+            if(cmd.containsKey("color")){
+                auto rgb = cmd["color"];
+
+                for(int i=0; i<strip.numPixels();i++){
+                    strip.setPixelColor(i,atol(rgb));
+                }
+            }
+            if(cmd.containsKey("brightness")){
+                auto brgt = cmd["brightness"];
+                strip.setBrightness(atol(brgt));
+            }
+            strip.show();
+           }else{
+            Serial.print("Couldnt parse Json Object from: ");
+            Serial.println(topic);
+            Serial.print("Error: ");
+            Serial.println(err.c_str());
+
+                    }
+           } */
+        sprintf(buffer, "%s%s%s", "/actu/", DEVICENAME, "/color");
+        if (!strcmp(buffer, topic)) {
+                char p[length + 1];
+                memcpy(p, payload, length);
+                p[length] = '/0';
+                uint32_t color = atol((char*)p);
+                for(int i=0; i<strip.numPixels(); i++) {
+                        strip.setPixelColor(i,color);
+                }
+                strip.show();
+                client.publish("/actu/LDL/status","color change");
+                client.publish("/actu/LDL/statusB",p);
+                client.publish("/actu/LDL/statusC",String(color));
+        }
+        sprintf(buffer, "%s%s%s", "/actu/", DEVICENAME, "/brightness");
+        if (!strcmp(buffer, topic)) {
+                char p[length + 1];
+                snprintf(p, length + 1, "%s", payload);
+                p[length] = NULL;
+                uint32_t brightness = atol(p);
+                strip.setBrightness(brightness);
+                strip.show();
+                client.publish("/actu/LDL/status","brightness change");
+                client.publish("/actu/LDL/statusp",p);
+                client.publish("/actu/LDL/statusb",String(brightness));
+        }
 
 }
 
