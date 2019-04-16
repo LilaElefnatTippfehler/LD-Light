69903e4698fdc14e0d8b6faa8566675d7d467c76
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2019-04-16 11:22:42 +0200

    added functions for compability also corrected a bug with low brightness

--- src/NeoPixel_wrapper.cpp
+++ src/NeoPixel_wrapper.cpp
@@ -12,6 +12,7 @@ void NeoPixel_wrapper::show(void){
 }
 void NeoPixel_wrapper::setPin(uint8_t p){
         this->strip.setPin(p);
+        this->currentColor.setRGBL((float) 0, (float) 0, (float) 0,(float) 255);
 }
 void NeoPixel_wrapper::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b){
         this->currentColor.setRGBL(r,g,b,currentColor.getIntL());
@@ -20,8 +21,15 @@ void NeoPixel_wrapper::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b
         }
 }
 void NeoPixel_wrapper::setPixelColor(uint8_t r, uint8_t g, uint8_t b){
-        this->setPixelColor(0, r, g, b);
+        this->setPixelColor((uint16_t)0,(uint8_t) r,(uint8_t) g,(uint8_t) b);
 }
+void NeoPixel_wrapper::setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w){
+        //not supporting this function atm;
+}
+void NeoPixel_wrapper::setPixelColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w){
+        this->setPixelColor((uint16_t)0,(uint8_t)r,(uint8_t)g,(uint8_t)b,(uint8_t)w);
+}
+
 void NeoPixel_wrapper::setPixelColor(uint16_t n, uint32_t c){
         uint8_t lum = currentColor.getIntL();
         this->currentColor.setRGBL(c);
@@ -33,9 +41,46 @@ void NeoPixel_wrapper::setPixelColor(uint16_t n, uint32_t c){
 void NeoPixel_wrapper::setPixelColor(uint32_t c){
         this->setPixelColor(0, c);
 }
+
+void NeoPixel_wrapper::setPixelColor(colorPoint c){
+        this->setPixelColor(c.getIntR(), c.getIntG(), c.getIntB());
+}
 void NeoPixel_wrapper::setBrightness(uint8_t l){
-        this->currentColor.setL(l);
-        this->strip.setBrightness(l);
+        if(currentColor.getL() <= 20) {
+                this->currentColor.setL(l);
+                this->strip.setBrightness(l);
+                this->setPixelColor(this->currentColor);
+        }else{
+                this->currentColor.setL(l);
+                this->strip.setBrightness(l);
+        }
+}
+void NeoPixel_wrapper::setColor(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue){
+        this->setPixelColor(aLedNumber, (uint8_t)aRed, (uint8_t)aGreen, (uint8_t)aBlue);
+}
+void NeoPixel_wrapper::setColor(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aWhite){
+        this->setPixelColor(aLedNumber, (uint8_t)aRed, (uint8_t)aGreen, (uint8_t)aBlue, (uint8_t)aWhite);
+}
+void NeoPixel_wrapper::setColorScaled(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aScaling){
+        //not supporting this function atm;
+}
+void NeoPixel_wrapper::setColorScaled(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aWhite, byte aScaling){
+        //not supporting this function atm;
+}
+void NeoPixel_wrapper::setColorDimmed(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aBrightness){
+        this->setPixelColor(aLedNumber, (uint8_t)aRed, (uint8_t)aGreen, (uint8_t)aBlue);
+        this->setBrightness((uint8_t) aBrightness);
+}
+void NeoPixel_wrapper::setColorDimmed(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aWhite, byte aBrightness){
+        this->setPixelColor(aLedNumber, (uint8_t)aRed, (uint8_t)aGreen, (uint8_t)aBlue, (uint8_t)aWhite);
+        this->setBrightness((uint8_t) aBrightness);
+}
+void NeoPixel_wrapper::updateLength(uint16_t n){
+        this->strip.updateLength(n);
+}
+void NeoPixel_wrapper::clear(void){
+        this->currentColor.setRGBL((uint8_t) 0,(uint8_t) 0,(uint8_t) 0,(uint8_t) 0);
+        this->strip.clear();
 }
 uint8_t NeoPixel_wrapper::getBrightness(){
         return this->currentColor.getL();
@@ -46,9 +91,21 @@ uint8_t *NeoPixel_wrapper::getPixels(){
 uint16_t NeoPixel_wrapper::numPixels(void){
         return this->strip.numPixels();
 }
+uint16_t NeoPixel_wrapper::getNumLeds(void){
+        return this->strip.getNumLeds();
+}
 uint32_t NeoPixel_wrapper::Color(uint8_t r, uint8_t g, uint8_t b){
         return this->strip.Color(r,g,b);
 }
+uint32_t NeoPixel_wrapper::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w){
+        return this->strip.Color(r,g,b,w);
+}
+uint32_t NeoPixel_wrapper::getPixelColor(uint16_t n){
+        return this->currentColor.getRGB();
+}
 colorPoint NeoPixel_wrapper::getPixelColor() {
         return colorPoint(this->currentColor);
+}
+byte NeoPixel_wrapper::brightnessToPWM(byte aBrightness){
+        return (uint8_t) aBrightness;
 }
--- src/NeoPixel_wrapper.h
+++ src/NeoPixel_wrapper.h
@@ -1,6 +1,7 @@
 #ifndef NEOPIXELWRAPPER_H_
 #define NEOPIXELWRAPPER_H_
 
+#include <stdint.h>
 #include <neopixel.h>
 #include "colorPoint.h"
 
@@ -19,14 +20,31 @@ void show(void);
 void setPin(uint8_t p);
 void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
 void setPixelColor(uint8_t r, uint8_t g, uint8_t b);
+void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+void setPixelColor(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
 void setPixelColor(uint16_t n, uint32_t c);
 void setPixelColor(uint32_t c);
+void setPixelColor(colorPoint c);
 void setBrightness(uint8_t);
+void setColor(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue);
+void setColor(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aWhite);
+void setColorScaled(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aScaling);
+void setColorScaled(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aWhite, byte aScaling);
+void setColorDimmed(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aBrightness);
+void setColorDimmed(uint16_t aLedNumber, byte aRed, byte aGreen, byte aBlue, byte aWhite, byte aBrightness);
+void updateLength(uint16_t n);
+void clear(void);
+
 uint8_t getBrightness();
 uint8_t *getPixels();
 uint16_t numPixels(void);
+uint16_t getNumLeds(void);
 uint32_t Color(uint8_t r, uint8_t g, uint8_t b);
+uint32_t Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+uint32_t getPixelColor(uint16_t n);
 colorPoint getPixelColor();
+
+byte brightnessToPWM(byte aBrightness);
 };
 
 #endif
